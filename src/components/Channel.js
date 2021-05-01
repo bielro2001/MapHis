3a592d34fba838669c459cd4cd54f42f91448145
bielro2001 <83517319+bielro2001@users.noreply.github.com>
2021-05-01 18:35:08 -0300

    Add files via upload

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -55,11 +55,11 @@ const Channel = ({ user = null }) => {
         <div className="py-4 max-w-screen-lg mx-auto">
           <div className="border-b dark:border-gray-600 border-gray-200 py-8 mb-4">
             <div className="font-bold text-3xl text-center">
-              <p className="mb-1">Welcome to</p>
-              <p className="mb-3">React FireChat</p>
+              <p className="mb-1"></p>
+              <p className="mb-3"></p>
             </div>
             <p className="text-gray-400 text-center">
-              This is the beginning of this chat.
+              Esse é o início do histórico
             </p>
           </div>
           <ul>
@@ -73,30 +73,11 @@ const Channel = ({ user = null }) => {
                 </li>
               ))}
           </ul>
-          <div ref={bottomListRef} />
+          <div/>
         </div>
       </div>
       <div className="mb-6 mx-4">
-        <form
-          onSubmit={handleOnSubmit}
-          className="flex flex-row bg-gray-200 dark:bg-coolDark-400 rounded-md px-4 py-3 z-10 max-w-screen-lg mx-auto dark:text-white shadow-md"
-        >
-          <input
-            ref={inputRef}
-            type="text"
-            value={newMessage}
-            onChange={handleOnChange}
-            placeholder="Type your message here..."
-            className="flex-1 bg-transparent outline-none"
-          />
-          <button
-            type="submit"
-            disabled={!newMessage}
-            className="uppercase font-semibold text-sm tracking-wider text-gray-500 hover:text-gray-900 dark:hover:text-white transition-colors"
-          >
-            Send
-          </button>
-        </form>
+  
       </div>
     </div>
   );
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { formatRelative } from 'date-fns';
-
+import { pt } from 'date-fns/locale';
+var resultado = "";
 const formatDate = date => {
   let formattedDate = '';
   if (date) {
     // Convert the date in words relative to the current date
-    formattedDate = formatRelative(date, new Date());
+    formattedDate = formatRelative(date, new Date(),{locale: pt});
     // Uppercase the first letter
     formattedDate =
       formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
@@ -19,9 +20,10 @@ const Message = ({
   text = '',
   displayName = '',
   photoURL = '',
+ 
 }) => {
   if (!text) return null;
-
+  {resultado = text.split(/\s*;\s*/)}
   return (
     <div className="px-4 py-4 rounded-md hover:bg-gray-50 dark:hover:bg-coolDark-600 overflow-hidden flex items-start">
       {photoURL ? (
@@ -41,12 +43,21 @@ const Message = ({
           {createdAt?.seconds ? (
             <span className="text-gray-500 text-xs">
               {formatDate(new Date(createdAt.seconds * 1000))}
-            </span>
+           </span>
           ) : null}
         </div>
-        <p>{text}</p>
+      
+        <p>{resultado[0]}</p>
+        <h1> --</h1>
+        <strong>Localização Atual:</strong>
+        <p>Latitude: {resultado[1]} </p>
+        <p>Longitude: {resultado[2]}  </p>
+        <h1> --</h1>
+       <font color="blue">
+       <p><a href={'https://www.google.com/maps/?q=' +  resultado[1] + resultado[2]} target="_blank" title="Abrir em nova janela">  <b>ABRIR A LOCALIZAÇÃO</b></a></p>
+       </font> 
       </div>
-    </div>
+   </div>
   );
 };
 
